c2e9d4c6c92cc2a52e99c27e763a942cea503e2f
Anirudh Kachroo <anirudh@pop-os.localdomain>
2020-08-07 10:26:44 +0530

    add body parsing middleware

--- index.js
+++ index.js
@@ -4,7 +4,38 @@ const app = express();
 
 
 app.get("/",(req,res)=>{
-    res.send("Hi there");
+    res.send(`
+    <div>
+    <form method = "POST">
+    <input name="name" placeholder="name">
+    <input name = "password" placeholder ="pass">
+    <button type  = "submit">Submit</button>
+    </form>
+    </div>
+    
+    `);
+});
+
+const bodyParser = (req,res,next) => {
+    if(req.method ==="POST"){
+        reqData = {};
+        req.on('data',data=>{
+        const parsed = data.toString('utf8').split('&');
+        for(let pair of parsed){
+            const [key,value] = pair.split('=');
+            reqData[key] = value
+        }
+        req.body = reqData;
+        next();
+    });
+    }else{
+        next();
+    }
+}
+
+app.post("/",bodyParser,(req,res)=>{
+    console.log(req.body);
+    res.send("Account Created");
 });
 
 app.listen(3000,()=>{
