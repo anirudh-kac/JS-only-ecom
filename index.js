9d34bb47682b0d0e8dd222f3e6ffdc1bcea3d4cd
Anirudh Kachroo <anirudh@pop-os.localdomain>
2020-08-11 13:36:13 +0530

    hash and salt passwords

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(cookieSession({
 // app.use(bodyParser);
 
 
-app.get("/",(req,res)=>{
+app.get("/signup",(req,res)=>{
     res.send(`
     <div>
     Your id is ${req.session.userId}
@@ -47,7 +47,7 @@ app.get("/",(req,res)=>{
 
 
 
-app.post("/",async (req,res)=>{
+app.post("/signup",async (req,res)=>{
     //console.log(req.body);
     const {email,password,passwordConfirmation} = req.body;
     const existingUser = await usersRepo.getOneBy({email});
@@ -67,6 +67,39 @@ app.post("/",async (req,res)=>{
     res.send("Account Created");
 });
 
+app.get('/signout',(req,res)=>{
+    req.session = null;
+    res.send('You are logged out');
+});
+
+app.get('/signin',(req,res)=>{
+    res.send(`
+    <div>
+    <form method = "POST">
+    <input name="email" placeholder="email">
+    <input name = "password" placeholder ="pass">
+    <button type  = "submit">Sign In</button>
+    </form>
+    </div>
+
+    `);
+});
+
+app.post("/signin",async (req,res)=>{
+    const {email,password} = req.body;
+    const user = await usersRepo.getOneBy({email});
+    if(!user){
+        return res.send("Email not found");
+    }
+
+    if(user.password!==password){
+        return res.send("Invalid Password");
+    }
+
+    req.session.userId = user.id;
+
+    res.send("You are signed in");
+});
 app.listen(3000,()=>{
     console.log("Listening");
 });
--- repositories/users.js
+++ repositories/users.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const crypto = require('crypto');
+const util = require('util');
+
+const scrypt = util.promisify(crypto.scrypt);
 
 class UsersRepository {
     constructor(filename){
@@ -24,11 +27,20 @@ class UsersRepository {
     }
 
     async create(attrs){
+        //{email,password}
         attrs.id = this.randomID();
+        const salt = crypto.randomBytes(8).toString('hex');
+        const buf = await scrypt(attrs.password,salt,62);
+
         const records = await this.getAll();
-        records.push(attrs);
+        const record = {
+            ...attrs,
+            password : `${buf.toString('hex')}.${salt}`
+        }
+        records.push(record);
+
         this.writeAll(records);
-        return attrs;
+        return record;
     }
 
     async writeAll(records){
@@ -78,6 +90,7 @@ class UsersRepository {
             }
         }
     }
+
 }
 
 module.exports = new UsersRepository('users.json');
