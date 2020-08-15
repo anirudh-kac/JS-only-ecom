88b1481eeadb2928d73de90a54070b3cf62e0592
Anirudh Kachroo <anirudh@pop-os.localdomain>
2020-08-15 11:03:41 +0530

    add layout

--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,15 +1,16 @@
-module.exports = ({req}) => {
-    return `
+const layout = require("../layout");
 
-    <div>
-    Your id is ${req.session.userId}
-    <form method = "POST">
-    <input name="email" placeholder="email">
-    <input name = "password" placeholder ="pass">
-    <input name = "passwordConfirmation" placeholder ="pass confirm">
-    <button type  = "submit">Submit</button>
-    </form>
-    </div>
+module.exports = ({req}) => {
+    return layout({content : `
+        <div>
+        Your id is ${req.session.userId}
+        <form method = "POST">
+        <input name="email" placeholder="email">
+        <input name = "password" placeholder ="pass">
+        <input name = "passwordConfirmation" placeholder ="pass confirm">
+        <button type  = "submit">Submit</button>
+        </form>
+        </div>
     
-    `;
+    `});
 }
