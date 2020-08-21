99a223dc9a5e117fd1f3249443dcafb77fa71856
Anirudh Kachroo <anirudh@pop-os.localdomain>
2020-08-21 11:46:57 +0530

    sign in validation

--- routes/admin/validators.js
+++ routes/admin/validators.js
@@ -11,11 +11,13 @@ module.exports = {
                             if(existingUser){
                                 throw new Error("Email in use");
                             }
-                        } ),
+                        } )
+                        .withMessage('Must be a valid email'),
 
     requirePassword : check('password')
                             .trim()
-                            .isLength({min:4,max:20}),
+                            .isLength({min:4,max:20})
+                            .withMessage("Must be between 4 and 20 characters"),
 
     requirePasswordConfirmation : check("passwordConfirmation")
                                         .trim()
