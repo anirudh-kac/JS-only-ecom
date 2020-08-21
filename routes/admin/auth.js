395b2c96b06c03207b182dca635f4e519a143564
Anirudh Kachroo <anirudh@pop-os.localdomain>
2020-08-21 10:27:12 +0530

    seperate validator

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -5,7 +5,7 @@ const usersRepo = require('../../repositories/users');
 const router = express.Router();
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
-
+const {requireEmail} = require('./validators')
 router.get("/signup",(req,res)=>{
     res.send(signupTemplate({req}));
 });
@@ -13,20 +13,9 @@ router.get("/signup",(req,res)=>{
 
 
 router.post("/signup",[
-
-    check('email').trim().normalizeEmail().isEmail().custom(async(email) => {
-        const existingUser = await usersRepo.getOneBy({email});
-        if(existingUser){
-            throw new Error("Email in use");
-        }
-    } ),
-    check('password').trim().isLength({min:4,max:20}),
-    check("passwordConfirmation").trim().isLength({min:4,max:20}).custom((passwordConfirmation,{req})=>{
-        if(passwordConfirmation !== req.body.password){
-            throw new Error('Passwords must match');
-        }
-    })
-
+    requireEmail,
+    requirePassword,
+    requirePasswordConfirmation
 ],async (req,res)=>{
     const errors = validationResult(req);
     console.log(errors);
