7df2430260771e253f87807fa51bf06a1ca2d41d
Anirudh Kachroo <anirudh@pop-os.localdomain>
2020-08-22 12:54:15 +0530

    validation for new product

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,14 +1,26 @@
 const express = require('express');
 
+const {validationResult} =  require('express-validator');
+const productsRepo = require('../../repositories/products');
 const router = express.Router();
-
+const {requireTitle , requirePrice} = require('./validators');
+const productsNewTemplate = require('../../views/admin/products/new');
 router.get('/admin/products',(req,res)=>{
 
 });
 
 
 router.get('/admin/products/new',(req,res)=>{
-    
+    return res.send(productsNewTemplate({}));
 });
+
+router.post('/admin/products/new',[requireTitle,requirePrice],(req,res)=>{
+
+    const errors = validationResult(req);
+    console.log(errors);
+    return res.send('submitted');
+})
+
+
 
 module.exports  = router;
